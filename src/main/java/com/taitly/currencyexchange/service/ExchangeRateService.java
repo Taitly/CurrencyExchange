fff6028ef251c1232f9505297a8f92ea6b2fd2e0
Taitly <kilroynik@gmail.com>
2025-02-14 18:17:16 +0300

    feat: add ExchangeRateService for management exchange rates (validation and exception handling are pending)

---
+++ src/main/java/com/taitly/currencyexchange/service/ExchangeRateService.java
@@ -0,0 +1,67 @@
+package com.taitly.currencyexchange.service;
+
+import com.taitly.currencyexchange.dao.CurrencyDao;
+import com.taitly.currencyexchange.dao.ExchangeRateDao;
+import com.taitly.currencyexchange.dto.ExchangeRateDto;
+import com.taitly.currencyexchange.entity.Currency;
+import com.taitly.currencyexchange.entity.ExchangeRate;
+import com.taitly.currencyexchange.mapper.ExchangeRateMapper;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class ExchangeRateService {
+    private static final ExchangeRateService INSTANCE = new ExchangeRateService();
+    private final ExchangeRateDao exchangeRateDao = ExchangeRateDao.getInstance();
+    private final ExchangeRateMapper exchangeRateMapper = ExchangeRateMapper.getInstance();
+    private final CurrencyDao currencyDao = CurrencyDao.getInstance();
+
+    public List<ExchangeRateDto> findAll() {
+     return exchangeRateDao.findAll().stream().map(exchangeRateMapper :: toDto).collect(Collectors.toList());
+    }
+
+    public ExchangeRateDto findByPairCode(String pairCode) {
+        String baseCurrencyCode = pairCode.substring(0, 3);
+        String targetCurrencyCode = pairCode.substring(3, 6);
+
+        Optional<ExchangeRate> exchangeRateOptional = exchangeRateDao.findByPairCode(baseCurrencyCode, targetCurrencyCode);
+        ExchangeRate exchangeRate = exchangeRateOptional.orElseThrow(() -> new RuntimeException("ExchangeRate not found"));
+
+        return exchangeRateMapper.toDto(exchangeRate);
+    }
+
+    public ExchangeRateDto create(String baseCurrencyCode, String targetCurrencyCode, String rateValue) {
+        Currency baseCurrency = currencyDao.findByCode(baseCurrencyCode)
+                .orElseThrow(() -> new RuntimeException("Base currency not found in database"));
+        Currency targetCurrency = currencyDao.findByCode(targetCurrencyCode)
+                .orElseThrow(() -> new RuntimeException("Base currency not found in database"));
+        BigDecimal rate = new BigDecimal(rateValue);
+
+        ExchangeRate exchangeRate = new ExchangeRate(null, baseCurrency, targetCurrency, rate);
+        ExchangeRate createdExchangeRate = exchangeRateDao.create(exchangeRate);
+
+        return exchangeRateMapper.toDto(createdExchangeRate);
+    }
+
+    public ExchangeRateDto update(String pairCode, String rateValue) {
+        BigDecimal rate = new BigDecimal(rateValue);
+
+        ExchangeRateDto exchangeRateDto = findByPairCode(pairCode);
+        ExchangeRate exchangeRate = exchangeRateMapper.toEntity(exchangeRateDto);
+        exchangeRate.setRate(rate);
+
+        ExchangeRate updatedExchangeRate = exchangeRateDao.update(exchangeRate);
+
+        return exchangeRateMapper.toDto(updatedExchangeRate);
+    }
+
+    public static ExchangeRateService getInstance() {
+        return INSTANCE;
+    }
+
+    private ExchangeRateService() {
+
+    }
+}
