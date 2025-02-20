b58c8ee3e6d0e7b808e85bce96d01f80d48380e9
Taitly <kilroynik@gmail.com>
2025-02-20 16:55:56 +0300

    feat: add CurrencyExchangeServlet for currency exchange

---
+++ src/main/java/com/taitly/currencyexchange/servlet/CurrenciesExchangeServlet.java
@@ -0,0 +1,35 @@
+package com.taitly.currencyexchange.servlet;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.taitly.currencyexchange.dto.CurrencyExchangeDto;
+import com.taitly.currencyexchange.service.CurrencyExchangeService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/exchange")
+public class CurrenciesExchangeServlet extends HttpServlet {
+    CurrencyExchangeService currencyExchangeService = CurrencyExchangeService.getInstance();
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+
+        try(PrintWriter printWriter = resp.getWriter()) {
+            String fromCode = req.getParameter("from");
+            String toCode = req.getParameter("to");
+            String amount = req.getParameter("amount");
+
+            CurrencyExchangeDto currencyExchangeDto = currencyExchangeService.exchange(fromCode, toCode, amount);
+            String json = objectMapper.writeValueAsString(currencyExchangeDto);
+            printWriter.write(json);
+        }
+    }
+}
