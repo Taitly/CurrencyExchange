86041c25229f57447e991dc87a10f1953aee9204
Taitly <kilroynik@gmail.com>
2025-02-25 18:05:28 +0300

    feat: enhance CurrencyServlet and CurrenciesServlet with error handling and HTTP response codes

--- src/main/java/com/taitly/currencyexchange/servlet/CurrenciesServlet.java
+++ src/main/java/com/taitly/currencyexchange/servlet/CurrenciesServlet.java
@@ -3,6 +3,9 @@ package com.taitly.currencyexchange.servlet;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.taitly.currencyexchange.dto.CurrencyDto;
 import com.taitly.currencyexchange.entity.Currency;
+import com.taitly.currencyexchange.exception.DataAlreadyExistsException;
+import com.taitly.currencyexchange.exception.DatabaseException;
+import com.taitly.currencyexchange.exception.InvalidDataException;
 import com.taitly.currencyexchange.mapper.CurrencyMapper;
 import com.taitly.currencyexchange.service.CurrencyService;
 import jakarta.servlet.ServletException;
@@ -14,23 +17,30 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
+import java.util.Map;
 
 @WebServlet("/currencies")
 public class CurrenciesServlet extends HttpServlet {
     private final CurrencyService currencyService = CurrencyService.getInstance();
 
     private final ObjectMapper objectMapper = new ObjectMapper();
-    private final CurrencyMapper currencyMapper =  CurrencyMapper.getInstance();
+    private final CurrencyMapper currencyMapper = CurrencyMapper.getInstance();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
+        PrintWriter printWriter = resp.getWriter();
 
-        try (PrintWriter printWriter = resp.getWriter()) {
+        try {
             List<CurrencyDto> currencies = currencyService.findAll();
+            resp.setStatus(HttpServletResponse.SC_OK);
             String json = objectMapper.writeValueAsString(currencies);
             printWriter.write(json);
+        } catch (DatabaseException e) {
+            writeErrorResponse(resp, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage(), printWriter);
+        } finally {
+            printWriter.close();
         }
     }
 
@@ -38,15 +48,33 @@ public class CurrenciesServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
+        PrintWriter printWriter = resp.getWriter();
 
-        try (PrintWriter printWriter = resp.getWriter()) {
+        try {
             String code = req.getParameter("code");
             String name = req.getParameter("name");
             String sign = req.getParameter("sign");
             Currency currencyToCreate = new Currency(null, code, name, sign);
 
             CurrencyDto currency = currencyService.create(currencyMapper.toDto(currencyToCreate));
-            printWriter.write(objectMapper.writeValueAsString(currency));
+            resp.setStatus(HttpServletResponse.SC_CREATED);
+            String json = objectMapper.writeValueAsString(currency);
+            printWriter.write(json);
+        } catch (InvalidDataException e) {
+            writeErrorResponse(resp, HttpServletResponse.SC_BAD_REQUEST, e.getMessage(), printWriter);
+        } catch (DataAlreadyExistsException e) {
+            writeErrorResponse(resp, HttpServletResponse.SC_CONFLICT, e.getMessage(), printWriter);
+        } catch (DatabaseException e) {
+            writeErrorResponse(resp, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage(), printWriter);
+        } finally {
+            printWriter.close();
         }
+    }
+
+    private void writeErrorResponse(HttpServletResponse resp, int errorCode, String errorMessage, PrintWriter printWriter) throws IOException {
+        resp.setStatus(errorCode);
+        Map<String, String> errorResponse = Map.of("message", errorMessage);
+        String json = objectMapper.writeValueAsString(errorResponse);
+        printWriter.write(json);
     }
 }
--- src/main/java/com/taitly/currencyexchange/servlet/CurrencyServlet.java
+++ src/main/java/com/taitly/currencyexchange/servlet/CurrencyServlet.java
@@ -2,6 +2,9 @@ package com.taitly.currencyexchange.servlet;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.taitly.currencyexchange.dto.CurrencyDto;
+import com.taitly.currencyexchange.exception.DataNotFoundException;
+import com.taitly.currencyexchange.exception.DatabaseException;
+import com.taitly.currencyexchange.exception.InvalidDataException;
 import com.taitly.currencyexchange.service.CurrencyService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -11,6 +14,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map;
 
 @WebServlet("/currency/*")
 public class CurrencyServlet extends HttpServlet {
@@ -21,12 +25,29 @@ public class CurrencyServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
+        PrintWriter printWriter = resp.getWriter();
 
-        try(PrintWriter printWriter = resp.getWriter()) {
+        try {
             String code = req.getPathInfo().substring(1);
             CurrencyDto currencyDto = currencyService.findByCode(code);
+            resp.setStatus(HttpServletResponse.SC_OK);
             String json = objectMapper.writeValueAsString(currencyDto);
             printWriter.write(json);
+        } catch (InvalidDataException e) {
+            writeErrorResponse(resp, HttpServletResponse.SC_BAD_REQUEST, e.getMessage(), printWriter);
+        } catch (DataNotFoundException e) {
+            writeErrorResponse(resp, HttpServletResponse.SC_NOT_FOUND, e.getMessage(), printWriter);
+        } catch (DatabaseException e) {
+            writeErrorResponse(resp, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage(), printWriter);
+        } finally {
+            printWriter.close();
         }
+    }
+
+    private void writeErrorResponse(HttpServletResponse resp, int errorCode, String errorMessage, PrintWriter printWriter) throws IOException {
+        resp.setStatus(errorCode);
+        Map<String, String> errorResponse = Map.of("message", errorMessage);
+        String json = objectMapper.writeValueAsString(errorResponse);
+        printWriter.write(json);
     }
 }
